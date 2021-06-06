0d4cf091fcee3b20fd3a7f71ffee28f2884757c6
ChiragShangle <shanglechiru3@gmail.com>
2021-06-06 20:26:42 +0530

    edit details page done

--- assets/scripts/Home.js
+++ assets/scripts/Home.js
@@ -3,3 +3,12 @@ const ScrollHandler = (ElemID) => {
   console.log(elem);
   elem.scrollIntoView();
 };
+
+i = 0;
+                  var x = document.getElementsByClassName("added");
+                    function addskill(){
+                          x[i].style.display = "block";
+                          i++;
+                        document.getElementById("remove").style.display = "inline";
+                    }
+                  
